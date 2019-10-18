54e5b9440a1bbe381eca2c762fcc966657008d1b
zgy <718138559@qq.com>
2019-10-18 14:02:36 +0800

    修改readme 删除多余注释

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # wenkuProject
 
-文库生成 doc 小工具
+baidu 文库生成 doc 小工具
+
+> 用 golang 重新写的 Lz1y 大佬的[FreeForWenku](https://github.com/Lz1y/FreeForWenku)项目
+
+文档转换在 functions/getDoc.go 中实现
--- main.go
+++ main.go
@@ -4,14 +4,4 @@ import "wenkuProject/network"
 
 func main() {
 	network.StartApi()
-	//url := "https://wenku.baidu.com/view/8635bcbbc4da50e2524de518964bcf84b8d52d58.html"
-
-	//log.Printf("输出文件:%v",docName)
-	//t := "doc"
-	//out,err := exec.Command("python", "pyScript/FreeForWenku.py",url, t).Output()
-	//if err != nil {
-	//	log.Println("运行python出错:",err)
-	//	return
-	//}
-	//log.Printf("out::::%s",string(out))
 }
